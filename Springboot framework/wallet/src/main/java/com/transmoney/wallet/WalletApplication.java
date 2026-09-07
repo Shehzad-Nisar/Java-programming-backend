4683b05214c26186b94c9c98e1b94cf077925c10
Shehzad-Nisar <shehzadnisar07@gmail.com>
2026-09-07 19:54:09 +0500

    controller

--- Springboot framework/wallet/src/main/java/com/transmoney/wallet/WalletApplication.java
+++ Springboot framework/wallet/src/main/java/com/transmoney/wallet/WalletApplication.java
@@ -12,8 +12,4 @@ public class WalletApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WalletApplication.class, args);
-	}
-
-	@Bean
-    CommandLineRunner dummyData(AccountService accountService) {
-		return args -> { accountService.createAccount( new Account("Ali Khan", "SAVINGS", 50000, true) ); accountService.createAccount( new Account("Ahmed Raza", "CURRENT", 125000, true) ); accountService.createAccount( new Account("Hassan Ali", "SAVINGS", 25000, true) ); accountService.createAccount( new Account("Bilal Hussain", "CURRENT", 80000, false) ); }; } }
+	} }
--- Springboot framework/wallet/src/main/java/com/transmoney/wallet/controller/AccountController.java
+++ Springboot framework/wallet/src/main/java/com/transmoney/wallet/controller/AccountController.java
@@ -2,13 +2,15 @@ package com.transmoney.wallet.controller;
 
 import com.transmoney.wallet.model.Account;
 import com.transmoney.wallet.service.AccountService;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/account")
 public class AccountController {
     private final AccountService service;
 
@@ -16,7 +18,13 @@ public class AccountController {
         this.service = service;
     }
 
-    @GetMapping("/accounts")
+    @PostMapping("/create")
+    public String createAccount(){
+        service.createAccount(new Account("Shehzad Nisar","Current-Account",5000.0,true));
+        return "Account created.";
+    }
+
+    @GetMapping("/allaccounts")
     public List<Account> getAccount(){
        return service.getAllAccounts();
     }
